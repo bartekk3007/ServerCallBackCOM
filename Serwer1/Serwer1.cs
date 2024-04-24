d225d78ae136f6ea4ace5b549d7d14f548947b62
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-04-24 23:28:06 +0200

    Add files via upload

---
+++ Serwer1/Serwer1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using KSR_WCF2;
+
+namespace Serwer1
+{
+    public class Zadanie3 : IZadanie3
+    {
+        public void TestujZwrotny()
+        {
+            var callback = OperationContext.Current.GetCallbackChannel<IZadanie3Zwrotny>();
+            for (int x = 0; x < 31; x++)
+            {
+                callback.WolanieZwrotne(x, x * x * x - x * x);
+            }
+        }
+    }
+
+    public class Task3Server
+    {
+        public static void Main(string[] args)
+        {
+            var host = new ServiceHost(typeof(Zadanie3));
+            host.AddServiceEndpoint(typeof(IZadanie3), new NetNamedPipeBinding(), "net.pipe://localhost/ksr-wcf2-zad3");
+
+            host.Open();
+            Console.WriteLine("Host opened");
+            Console.ReadKey();
+
+            host.Close();
+            Console.WriteLine("Host closed");
+            Console.ReadKey();
+
+        }
+    }
+}
